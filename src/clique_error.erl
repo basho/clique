708e8c74b2b0ea7b4c350a32c968b51d3a3fe4b9
John R. Daily <git@epep.us>
2014-12-16 14:32:17 -0500

    Handle cuttlefish nested errors gracefully

--- src/clique_error.erl
+++ src/clique_error.erl
@@ -63,6 +63,12 @@ format({error, {invalid_config_keys, Invalid}}) ->
     status(io_lib:format("Invalid Config Keys: ~s~n", [Invalid]));
 format({error, config_no_args}) ->
     status("Config Operations require one or more arguments");
+format({error, {invalid_config, {error, [_H|_T]=Msgs}}}) ->
+    %% Cuttlefish deeply nested errors
+    status(lists:flatten(
+             lists:foldl(fun({error, Msg}, Acc) ->
+                                 Acc ++ io_lib:format("~ts~n", [Msg]) end,
+                         [], Msgs)));
 format({error, {invalid_config, Msg}}) ->
     status(io_lib:format("Invalid Configuration: ~p~n", [Msg]));
 format({error, bad_node}) ->
